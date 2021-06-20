57cdf24e803fc7df7bc1da64e82af0c282ee8868
wyokobe <wyokobe@users.noreply.github.com>
2021-06-20 16:26:34 +0900

    add timeout

--- narou_api/__init__.py
+++ narou_api/__init__.py
@@ -3,52 +3,52 @@ import sys
 import requests
 
 
-def get(**kwargs):
+def get(timeout=30.0, **kwargs):
     """
     なろう小説API
     @see https://dev.syosetu.com/man/api/
     """
     url = "https://api.syosetu.com/novelapi/api/"
-    return exec_get(url, **kwargs)
+    return exec_get(url, float(timeout), **kwargs)
 
 
-def get_rank(**kwargs):
+def get_rank(timeout=30.0, **kwargs):
     """
     なろう小説ランキングAPI
     @see https://dev.syosetu.com/man/rankapi/
     """
     url = "https://api.syosetu.com/rank/rankget/"
-    return exec_get(url, **kwargs)
+    return exec_get(url, float(timeout), **kwargs)
 
 
-def get_rankin(**kwargs):
+def get_rankin(timeout=30.0, **kwargs):
     """
     なろう殿堂入りAPI
     @see https://dev.syosetu.com/man/rankinapi/
     """
     url = "https://api.syosetu.com/rank/rankin/"
-    return exec_get(url, **kwargs)
+    return exec_get(url, float(timeout), **kwargs)
 
 
-def get_user(**kwargs):
+def get_user(timeout=30.0, **kwargs):
     """
     なろうユーザ検索API
     @see https://dev.syosetu.com/man/userapi/
     """
     url = "https://api.syosetu.com/userapi/api/"
-    return exec_get(url, **kwargs)
+    return exec_get(url, float(timeout), **kwargs)
 
 
-def getx(**kwargs):
+def getx(timeout=30.0, **kwargs):
     """
     なろうR18小説API
     @see https://dev.syosetu.com/xman/api/
     """
     url = "https://api.syosetu.com/novel18api/api/"
-    return exec_get(url, **kwargs)
+    return exec_get(url, float(timeout), **kwargs)
 
 
-def exec_get(url, **kwargs):
+def exec_get(url, timeout=30.0, **kwargs):
     """共通GETエクゼキューター"""
     is_first_loop = True
     for k, v in kwargs.items():
@@ -59,7 +59,7 @@ def exec_get(url, **kwargs):
             url += "&"
         url += f"{k}={v}"
 
-    return requests.get(url)
+    return requests.get(url, timeout=timeout)
 
 
 commands = locals()
